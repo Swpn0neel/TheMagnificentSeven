5c29bd09c31504c5b8fc213e959e0c4758ca5456
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-02-23 19:27:59 +0530

    Added Button Design
    Re-worked the button design of the Home Page completely and changed the texts of the Home Page.

--- src/App.svelte
+++ src/App.svelte
@@ -30,12 +30,11 @@
 
 <main bind:this={main}>
   <header bind:this={header} class="con-header">
-    <h1 bind:this={header_heading}>Magnificient Seven</h1>
+    <h1 bind:this={header_heading}>The Magnificient Seven</h1>
     <p bind:this={header_para}>
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page when looking at its layout.
+      A Deep-Dive into the World's magnificent structures built by the humans.
     </p>
-    <button bind:this={header_btn} on:click={toggleFullscreen}>Explore</button>
+    <button bind:this={header_btn} on:click={toggleFullscreen}><span>Explore</span></button>
   </header>
   <div class="con-canvas">
     <EarthCanvas/>
@@ -87,9 +86,56 @@
   }
 
   header > button {
-    font-weight: bolder;
+    /* font-weight: bolder;
     color: black;
     width: 100%;
-    padding: 1rem 2rem;
+    padding: 1rem 2rem; */
+    
+  align-items: center;
+  background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  display: flex;
+  font-family: Phantomsans, sans-serif;
+  font-size: 20px;
+  justify-content: center;
+  line-height: 1em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 3px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+}
+
+button:active,
+header > button:hover {
+  outline: 0;
+}
+
+header > button span {
+  background-color: rgb(5, 6, 45);
+  padding: 16px 24px;
+  border-radius: 6px;
+  width: 100%;
+  height: 100%;
+  transition: 300ms;
+}
+
+header > button:hover span {
+  background: none;
+}
+
+@media (min-width: 768px) {
+  button {
+    font-size: 24px;
+    min-width: 196px;
+  }
   }
 </style>
