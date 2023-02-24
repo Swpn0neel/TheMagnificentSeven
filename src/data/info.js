051fc5a6394b97db500c2c7caa3859b063d575c2
Samya brata Maji <samyabratamaji334@gmail.com>
2023-02-24 11:55:50 +0530

    data: great wall of china

---
+++ src/data/info.js
@@ -0,0 +1,19 @@
+const PLACES_DATA = [
+  {
+    pageOne: {
+      title: "The Great Wall of China",
+      description:
+        "The Great Wall of China is one of the most iconic landmarks in the world. It is a massive wall that stretches over 13,000 miles across the northern part of China. The wall was constructed over a period of many centuries, with various dynasties adding to it and modifying it. Today, it is one of the most popular tourist attractions in China, drawing millions of visitors from all over the world each year.",
+    },
+    pageTwo: {
+      title: "History",
+      description:
+        "The Great Wall of China was built over 2,000 years ago during the Warring States period to protect Chinese territories from invasions. The various walls were connected during the Qin dynasty to form a single, continuous wall to protect China from northern nomads. The wall was constructed using different materials and was modified and extended over the centuries by various dynasties, particularly by the Ming dynasty, who repaired and reinforced sections, and built new sections. The wall served not only for defense but also for border control and trade regulation.",
+    },
+    pageThree: {
+      title: "Features",
+      description:
+        "The Great Wall of China is an impressive engineering feat made of stone, brick, tamped earth, and wood. It is 15-30 feet wide and up to 26 feet tall, with the most famous sections built during the Ming dynasty using brick and stone. The wall has watchtowers, battlements, crenellations, and gates used for defense and access control.",
+    },
+  },
+];
